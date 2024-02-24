2d5d5d1f0953377907e5d87b93c2de1d68fbae34
sajjad-933 <sajjadmuhammad933@gmail.com>
2024-02-25 00:17:29 +0500

    newest

--- littlelemon/workspace/littlelemon/restaurant/urls.py
+++ littlelemon/workspace/littlelemon/restaurant/urls.py
@@ -1,6 +1,12 @@
 from django .urls import path
-from .import views
+#from .views import menuview, bookingview
+from .views import MenuItemsView
+from . import views
 
 urlpatterns = [
-    path('',views.index, name = 'index' ),
+    #path('',views.index, name = 'index' ),
+    #path('menu/', menuview.as_view()),
+    #path('booking/', bookingview.as_view()),
+    path('menu/', views.MenuItemsView.as_view()),
+    path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
 ]
--- littlelemon/workspace/littlelemon/restaurant/views.py
+++ littlelemon/workspace/littlelemon/restaurant/views.py
@@ -1,5 +1,43 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Booking, MenuItem
+from .serializers import BookingSerializer, MenuItemSerializer
+from rest_framework import generics
+from rest_framework.decorators import api_view
+from rest_framework import viewsets
+from rest_framework import permissions
+
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+'''class bookingview(APIView):
+    
+    def get(self, request):
+        items = Booking.object.all()
+        serializer = BookingSerializer(items, many=True)
+        return Response(serializer.data)
+    
+class menuview(APIView):
+    def post(self, request):
+        serrializer = MenuSerializer(data=request.data)
+        if serrializer.is_valid():
+            serrializer.save()
+            return Response({'status':'success', 'data':serializer.data})'''
+
+class MenuItemsView(generics.ListCreateAPIView):   
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+    
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    permission_classes = [permissions.IsAuthenticated]  
+        
+        
